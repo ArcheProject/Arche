66b6108b0e5581145cad27de5f40159182b1979c
robinharms <robin@betahaus.net>
2014-04-28 11:41:23 +0200

    Don't use values attr for non-folderish content

--- arche/views/contents.py
+++ arche/views/contents.py
@@ -1,3 +1,4 @@
+from arche.interfaces import IFolder
 from arche.views.base import BaseView
 from arche import security
 from arche import _
@@ -6,7 +7,9 @@ from arche import _
 class ContentsView(BaseView):
 
     def __call__(self):
-        return {'contents': self.context.values()}
+        if IFolder.providedBy(self.context):
+            return {'contents': self.context.values()}
+        return {'contents': ()}
 
 
 def includeme(config):
