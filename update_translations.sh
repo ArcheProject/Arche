996bdf411a78cdfcef8b17063bf3a93846c69f0f
robinharms <robin@betahaus.net>
2015-04-13 17:59:25 +0200

    Moved script + translation of reg instructions

--- update_translations.sh
+++ update_translations.sh
@@ -2,9 +2,9 @@
  #You need lingua and gettext installed to run this
  
  echo "Updating Arche.pot"
- pot-create -d Arche -o Arche.pot ../.
+ pot-create -d Arche -o arche/locale/Arche.pot arche/.
  echo "Merging Swedish localisation"
- msgmerge --update sv/LC_MESSAGES/Arche.po Arche.pot
+ msgmerge --update  arche/locale/sv/LC_MESSAGES/Arche.po  arche/locale/Arche.pot
  echo "Updated locale files"
  
  
