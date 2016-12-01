965920c494a978325ee891c7d4c762117ebc44b4
Robin Harms Oredsson <robin@betahaus.net>
2016-12-01 12:18:44 +0100

    Must check for folderish interface, since this view might be invoked on regular persistent objects

--- arche/views/tinymce.py
+++ arche/views/tinymce.py
@@ -1,17 +1,20 @@
-from arche.views.base import BaseView
 from pyramid.view import view_config
+from repoze.folder import IFolder
+
+from arche.views.base import BaseView
 
 
 class TinyMCEViews(BaseView):
     @view_config(name="contained_image_list.json", renderer='json')
     def external_image_list(self):
         images = []
-        for obj in self.context.values():
-            if obj.type_name != 'Image':
-                continue
-            url = self.request.resource_url(obj, 'inline')
-            images.append({'title': obj.title, 'value': url})
-        images.sort(key=lambda x: x['title'].lower())
+        if IFolder.providedBy(self.context):
+            for obj in self.context.values():
+                if obj.type_name != 'Image':
+                    continue
+                url = self.request.resource_url(obj, 'inline')
+                images.append({'title': obj.title, 'value': url})
+            images.sort(key=lambda x: x['title'].lower())
         return images
 
 
