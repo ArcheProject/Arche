7cb68993e30817c38929b83949149c74d75db61d
Robin Harms Oredsson <robin@betahaus.net>
2017-02-16 09:41:13 +0100

    Play nice with newer versions of sendmail

--- arche/testing.py
+++ arche/testing.py
@@ -60,6 +60,11 @@ class PrintingMailer(object):
 
     send_to_queue = send_immediately = send
 
+    @classmethod
+    def from_settings(cls, settings):
+        return cls()
+
+
 def includeme(config):
     """ Setup minimal basics for running tests. """
     config.include('betahaus.viewcomponent')
