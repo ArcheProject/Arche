f5ff4eb551c0a77f5669c076bde466b12c980e25
Robin Harms Oredsson <robin@betahaus.net>
2017-04-30 13:25:19 +0200

    Portlet manager fixes

--- arche/portlets/__init__.py
+++ arche/portlets/__init__.py
@@ -171,7 +171,6 @@ class PortletManager(IterableUserDict):
         return portlet
 
     def get_portlets(self, slot, portlet_type = None):
-       # [x.portlet_type for x in manager.get('agenda_item', {}).values()]
         results = []
         for portlet in self.get(slot, {}).values():
             if portlet_type and portlet_type != portlet.portlet_type:
@@ -208,17 +207,21 @@ def get_portlet_manager(context, registry = None):
         registry = get_current_registry()
     return registry.queryAdapter(context, IPortletManager)    
 
+
 def get_available_portlets(registry = None):
     if registry is None:
         registry = get_current_registry()
-    return [(x.name, x.name) for x in registry.registeredAdapters() if x.provided == IPortletType]
+    return [(x.name, x.factory.title) for x in registry.registeredAdapters() if x.provided == IPortletType]
+
 
 def add_portlet(config, portlet):
     verifyClass(IPortletType, portlet)
     config.registry.registerAdapter(portlet, name = portlet.name)
 
+
 def add_portlet_slot(config, name, title = "", layout = ""):
     config.registry.portlet_slots[name] = PortletSlotInfo(name, title = title, layout = layout)
+
 
 def includeme(config):
     config.add_content_factory(Portlet)
@@ -231,4 +234,3 @@ def includeme(config):
     config.add_portlet_slot('right', title = _("Right"), layout = 'vertical')
     config.add_portlet_slot('top', title = _("Top"), layout = 'horizontal')
     config.add_portlet_slot('bottom', title = _("Bottom"), layout = 'horizontal')
-    
