530e398ef03829d63b96b292e798904caf557b70
Johan Schiff <johan.schiff@mp.se>
2018-03-27 17:40:31 +0200

    Search js IE fix

--- arche/static/search.js
+++ arche/static/search.js
@@ -5,39 +5,30 @@ See templates/master.pt for example structure of livesearch.
 The #search form implements it.
 */
 
-class SearchWatcher {
-
-    constructor(search_selector) {
-        this.search_selector = search_selector;
-        this.tpl = null;
-        this.directive = {
-            'li':{
-                'obj<-results':{
-                    '[data-title]': 'obj.title',
-                    '[data-css-icon]@class+': 'obj.css_icon',
-                    'a@href': 'obj.url',
-                    'a@class+': function(a) {
-                        return " " + a.item.type_name;
-                    },
-                    '[data-img]@src': 'obj.thumb_url',
-                }
+function SearchWatcher (search_selector) {
+    this.search_selector = search_selector;
+    this.tpl = null;
+    this.directive = {
+        'li':{
+            'obj<-results':{
+                '[data-title]': 'obj.title',
+                '[data-css-icon]@class+': 'obj.css_icon',
+                'a@href': 'obj.url',
+                'a@class+': function(a) {
+                    return " " + a.item.type_name;
+                },
+                '[data-img]@src': 'obj.thumb_url',
             }
         }
-        this.delay_time = 1000;
-        this.hide_delay = 3000;
-        this.min_chars = 2;
-        this.timer = null;
-        this.timer_hide = null;
-        this.has_results = false;
-        $('body').on('input propertychange', this.search_selector, this.handle_change_event.bind(this));
-        this.target_name = $(this.search_selector).data('watch-search');
-        $('body').on('mouseleave', this.target_name, this.handle_mouseleave.bind(this));
-        $('body').on('mouseover', this.target_name, this.handle_mouseon.bind(this));
-        $('body').on('mouseleave', this.search_selector, this.handle_mouseleave.bind(this));
-        $('body').on('mouseover', this.search_selector, this.handle_mouseon_reopen.bind(this));
     }
+    this.delay_time = 1000;
+    this.hide_delay = 3000;
+    this.min_chars = 2;
+    this.timer = null;
+    this.timer_hide = null;
+    this.has_results = false;
 
-    search(search_field) {
+    this.search = function(search_field) {
         var form = search_field.parents('form')
         if (form.length == 1) {
             arche.actionmarker_feedback(form, true);
@@ -51,7 +42,7 @@ class SearchWatcher {
         }
     }
 
-    handle_change_event(event) {
+    this.handle_change_event = function(event) {
         if (this.timer != null) {
             clearTimeout(this.timer);
         }
@@ -64,7 +55,7 @@ class SearchWatcher {
         }
     }
 
-    handle_search_response(response) {
+    this.handle_search_response = function(response) {
         if (this.timer_hide != null) {
             clearTimeout(this.timer_hide);
         }
@@ -86,21 +77,21 @@ class SearchWatcher {
         //this.start_close_timer();
     }
 
-    handle_mouseon(event) {
+    this.handle_mouseon = function(event) {
         if (this.timer_hide != null) {
             clearTimeout(this.timer_hide);
         }
     }
 
-    handle_mouseleave(event) {
+    this.handle_mouseleave = function(event) {
         this.start_close_timer();
     }
 
-    handle_mouseon_reopen(event) {
+    this.handle_mouseon_reopen = function(event) {
         if (this.has_results) $(this.target_name).addClass('active');
     }
 
-    start_close_timer() {
+    this.start_close_timer = function() {
         if (this.timer_hide != null) {
             clearTimeout(this.timer_hide);
         }
@@ -109,12 +100,20 @@ class SearchWatcher {
         }.bind(this), this.hide_delay);
     }
 
-    static handle_close_results(event) {
-        event.preventDefault();
-        var target = $(event.currentTarget).data('close-results');
-        $(target).removeClass('active');
-    }
+    $('body').on('input propertychange', this.search_selector, this.handle_change_event.bind(this));
+    this.target_name = $(this.search_selector).data('watch-search');
+    $('body').on('mouseleave', this.target_name, this.handle_mouseleave.bind(this));
+    $('body').on('mouseover', this.target_name, this.handle_mouseon.bind(this));
+    $('body').on('mouseleave', this.search_selector, this.handle_mouseleave.bind(this));
+    $('body').on('mouseover', this.search_selector, this.handle_mouseon_reopen.bind(this));
 }
+
+SearchWatcher.handle_close_results = function(event) {
+    event.preventDefault();
+    var target = $(event.currentTarget).data('close-results');
+    $(target).removeClass('active');
+}
+
 
 
 $(document).ready(function() {
