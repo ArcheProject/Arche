44b4cc556bab6017226c629cbc49c06605a1daae
Robin Harms Oredsson <robin@betahaus.net>
2018-06-27 14:16:02 +0200

    Moved to a separate package instead

--- arche/plugins/tos/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-
-def includeme(config):
-    config.include('.models')
-    config.include('.resource')
-    config.include('.schemas')
-    config.include('.views')
--- arche/plugins/tos/exceptions.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-
-class TermsNeedAcceptance(Exception):
-    pass
-
-
-class TermsNotAccepted(Exception):
-    pass
